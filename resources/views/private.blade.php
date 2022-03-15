4ba4ae3468f4f5e75333fe1901c19a6ff8ed2d2d
patriciax <yively.pa@gmail.com>
2022-03-15 15:05:08 -0500

    Update private.blade.php

--- resources/views/private.blade.php
+++ resources/views/private.blade.php
@@ -3,17 +3,6 @@
 @section("content")
 <main class="bg-dark" data-barba="container" data-barba-namespace="private-section">
 
-<<<<<<< HEAD
-    <section class="main-private">
-        <div class="form">
-
-            <div class="content-form">
-                <h3>PRIVATE</h3>
-                <div class="mb-2">
-                    <label for="">User:</label>
-                    <input class="ml-27" type="text" autocomplete="off">
-                </div>
-=======
   <section class="main-private">
     <div class="form">
 
@@ -34,13 +23,9 @@
 
         </form>
       </div>
->>>>>>> faedabb1b62cb63fbed5e7a058f34d4f3cc5fef8
 
-                <div>
-                    <label for="">Password:</label>
-                    <input class="mb-2" class="mb-2" type="password">
-                </div>
-                <button class="btn">Submit</button>
+
+
             </div>
 
 
