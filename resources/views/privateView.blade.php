61b7535eedfa99d18b068e7e6cbfcf3ec2360f97
patriciax <yively.pa@gmail.com>
2022-03-22 21:00:52 -0500

    fix responsive

--- resources/views/privateView.blade.php
+++ resources/views/privateView.blade.php
@@ -1,41 +1,56 @@
 @extends("layouts.main")
 
 @section("content")
-<main class="bg-light" data-barba="container" data-barba-namespace="contact-section">
+<main class=" bg-dark--private" data-barba="container" data-barba-namespace="contact-section">
+    <div class="private-main">
+        <div class="item-private">
 
-    @foreach($secondaryContent as $content)
 
-        @php
+            @foreach($secondaryContent as $content)
+
+            @php
             $model = App\Models\Project::where('id', $content->project_id)->first();
-        @endphp
-       
-        <a href="{{ url('/private-project/'.$model->slug) }}">{{ $model->name }}</a> <br>
-        @if($model->image_type == 'image')
+            @endphp
 
-            <img src="{{ $model->image }}" alt="" class="w-100">
+            <a class="private-a" href="{{ url('/private-project/'.$model->slug) }}">{{ $model->name }}</a> <br>
+            @if($model->image_type == 'image')
 
-        @else
+            <!-- Use figure for a more semantic html -->
+            <div class="gallery">
+                <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                    <!-- Link to the big image, not mandatory, but usefull when there is no JS -->
+                    <a href="{{ $model->image }}" data-width="1200" data-height="900" itemprop="contentUrl">
+                        <!-- Thumbnail -->
+                        <img src="{{ $model->image }}" itemprop="thumbnail" alt="Image description">
+
+                    </a>
+                </figure>
+
+            </div>
+
+            @else
 
             <video class="w-100">
                 <source src="{{ $model->image }}">
             </video>
 
-        @endif
+            @endif
 
 
-    @endforeach
+            @endforeach
 
 
-
+        </div>
+    </div>
 </main>
 <style>
-
-    footer .copy, .main-logo img {
-display: none;
+    footer .copy {
+        display: none;
     }
-    footer{
+
+    footer {
         position: fixed;
-    right: 0;
+        right: 0;
     }
 </style>
 
