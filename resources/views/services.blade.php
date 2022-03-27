7723bcf0298e98ced771630e7eb0fbd9a9287c12
patriciax <yively.pa@gmail.com>
2022-03-27 10:45:12 -0500

    fix service

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -27,23 +27,19 @@
                     <div class="content ">
 
                         @foreach(App\Models\Photographer::where("type", "Location")->get() as $location)
-
-
                         <div id="tab-{{ $location->id }}" class="tab-content-general tab-content @if($loop->index == 0) current @endif">
-
-
                             <div class="name-gallery">
                                 <p> {{ strtoupper($location->name) }}</p>
                             </div>
                             <!-- Galley wrapper that contains all items -->
-                            <div id="gallery" class="gallery slider-servicess" itemscope itemtype="http://schema.org/ImageGallery">
+                            <div id="gallery" class="gallery slider-servicess" >
                                 @foreach(App\Models\PhotographerPicture::where("photographer_id", $location->id)->get() as $picture)
                                 <!-- Use figure for a more semantic html -->
-                                <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                                <figure itemprop="associatedMedia">
                                     <!-- Link to the big image, not mandatory, but usefull when there is no JS -->
-                                    <a href="{{ $picture->image }}" data-caption="Sea side, south shore<br><em class='text-muted'>© Dominik Schröder</em>" data-width="1200" data-height="900" itemprop="contentUrl">
+                                    <a href="{{ $picture->image }}" data-width="1200" data-height="900" >
                                         <!-- Thumbnail -->
-                                        <img src="{{ $picture->image }}" itemprop="thumbnail" alt="Image description" style="width: 300px; height: 300px;">
+                                        <img src="{{ $picture->image }}"  style="width: 300px; height: 300px;">
 
                                         <div class="marco" alt=""></div>
                                     </a>
@@ -74,15 +70,15 @@
                                 </p>
                             </div>
                             <!-- Galley wrapper that contains all items -->
-                            <div id="gallery" class="gallery slider-servicess" itemscope itemtype="http://schema.org/ImageGallery">
+                            <div id="gallery" class="gallery slider-servicess">
 
                                 @foreach(App\Models\PhotographerPicture::where("photographer_id", $location->id)->get() as $picture)
                                 <!-- Use figure for a more semantic html -->
-                                <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                                <figure itemprop="associatedMedia">
                                     <!-- Link to the big image, not mandatory, but usefull when there is no JS -->
-                                    <a href="{{ $picture->image }}" data-caption="Sea side, south shore<br><em class='text-muted'>© Dominik Schröder</em>" data-width="1200" data-height="900" itemprop="contentUrl">
+                                    <a href="{{ $picture->image }}"  data-width="1200" data-height="900">
                                         <!-- Thumbnail -->
-                                        <img src="{{ $picture->image }}" itemprop="thumbnail" alt="Image description" style="width: 300px; height: 300px;">
+                                        <img src="{{ $picture->image }}"  style="width: 300px; height: 300px;">
 
                                         <div class="marco" alt=""></div>
                                     </a>
