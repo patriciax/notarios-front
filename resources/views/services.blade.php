8365f9921811bf889fc83a13f82efadde2bfe60e
patriciax <yively.pa@gmail.com>
2022-03-27 14:55:52 -0500

    fix service

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -93,53 +93,18 @@
                                         <a href="#" class="modal__close">&times;</a>
                                     </div>
                                 </div>
-                                <script>
+                                <!--
+                                      @foreach(App\Models\PhotographerPicture::where("photographer_id", $location->id)->get() as $picture)
+                                       holisç
+                                       @endforeach
+                                    <a href="{{ $picture->image }}"  data-width="1200" data-height="900">
 
-/* full page carousel */
-$(document).ready(function () {
-    $(".carousel").carousel({
-        pause: "false",
-    });
-    $(".carousel").css({
-        margin: 0,
-        width: $(window).outerWidth(),
-        height: $(window).outerHeight(),
-    });
-    //$('.carousel-inner').css({'z-index': 0});
-    $(".carousel .item").css({
-        position: "fixed",
-        width: "100%",
-        height: "100%",
-    });
-    $(".carousel-inner div.itemimg").each(function () {
-        varimgSrc = $(this).attr("src");
-        $(this)
-            .parent()
-            .css({
-                background: "url(" + imgSrc + ") center center no-repeat",
-                "-webkit-background-size": "100% ",
-                "-moz-background-size": "100%",
-                "-o-background-size": "100%",
-                "background-size": "100%",
-                "-webkit-background-size": "cover",
-                "-moz-background-size": "cover",
-                "-o-background-size": "cover",
-                "background-size": "cover",
-            });
-        $(this).remove();
-    });
-    $(window).on("resize", function () {
-        $(".carousel").css({
-            width: $(window).outerWidth(),
-            height: $(window).outerHeight(),
-        });
-    });
-});
+                                        <img src="{{ $picture->image }}"  style="width: 300px; height: 300px;">
 
-                                </script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                                        <div class="marco" alt=""></div>
+                                    </a>
+                                </figure>---}}---->
+
 
 
                             </div>
@@ -150,7 +115,6 @@ $(document).ready(function () {
                 </div>
         </div>
     </div>
-
 
 
 
