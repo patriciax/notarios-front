64cb2dbbfde3dc67666f1f505f58eedb594b7714
patriciax <yively.pa@gmail.com>
2022-03-27 14:53:18 -0500

    fix service

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -94,6 +94,7 @@
                                     </div>
                                 </div>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
