a415822989282d883083f3af157c1434458ef60d
patriciax <yively.pa@gmail.com>
2022-03-27 12:34:58 -0500

    fix service

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -107,6 +107,7 @@
                                 </div>
                                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
                                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
                                 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
                                 <!--
                                     <a href="{{ $picture->image }}"  data-width="1200" data-height="900">
