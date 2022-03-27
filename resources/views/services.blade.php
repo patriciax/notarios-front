ec523400383e58162d58d0b96654f15432e1b4c1
patriciax <yively.pa@gmail.com>
2022-03-27 11:24:59 -0500

    fix service

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -32,7 +32,7 @@
                                 <p> {{ strtoupper($location->name) }}</p>
                             </div>
                             <!-- Galley wrapper that contains all items -->
-                            <div id="gallery" class="gallery slider-servicess" >
+                            <div id="gallery" class="gallery slider-servicess">
                                 @foreach(App\Models\PhotographerPicture::where("photographer_id", $location->id)->get() as $picture)
                                 <!-- Use figure for a more semantic html -->
                                 <figure>
@@ -74,45 +74,25 @@
 
 
                                 <div class="slider-modal">
-                                @foreach(App\Models\PhotographerPicture::where("photographer_id", $location->id)->get() as $picture)
-                                <!-- Use figure for a more semantic html -->
 
                                     <div class="close-icon modal-close"></div>
-
                                     <h5 class="title">Slider Modal</h5>
                                     <div class="slides">
+
                                         <div class="nav-arrow left"></div>
                                         <div class="nav-arrow right"></div>
-                                        <div class="slide active" title="Lion" style="background-image: url('https://unsplash.it/601/502')"> </div>
-                                        <div class="slide" title="Man in a church" style="background-image: url('https://unsplash.it/600/502')"> </div>
-                                        <div class="slide" title="Beach Sunset" style="background-image: url('https://unsplash.it/601/504')"> </div>
-                                        <div class="slide" title="Foggy forest" style="background-image: url('https://unsplash.it/601/503')"> </div>
-                                        <div class="slide" title="Girl reading in the woods" style="background-image: url('https://unsplash.it/601/506')"> </div>
+                                        @foreach(App\Models\PhotographerPicture::where("photographer_id", $location->id)->get() as $picture)
+                                        <!-- Use figure for a more semantic html -->
+                                        <div class="slide " title="Lion" style="background-image: url('{{ $picture->image }}')"> </div>
+                                        @endforeach
                                     </div>
                                     <p class="slider-image-caption">sdfsdfsdf</p>
-                                    @endforeach
-                                </div>
 
+                                </div>
                                 <div>
-                                    <!-- Link to the big image, not mandatory, but usefull when there is no JS -->
-
-                                    <!-- Thumbnail -->
                                     <button> <img src="{{ $picture->image }}" style="width: 300px; height: 300px;"></button>
-
                                     <div class="marco" alt=""></div>
-
                                 </div>
-
-
-
-
-
-
-
-
-
-
-
 
                             </div>
                             <div class="counter">1/</div>
